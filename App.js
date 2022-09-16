1320c95880930bf6258549587a501f1dd03f9f71
Bradley Service <bradleykservice@gmail.com>
2022-09-15 19:02:57 -0500

    WIP basic setup

--- App.js
+++ App.js
@@ -1,13 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as eva from '@eva-design/eva'
+import { StyleSheet, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { ApplicationProvider, BottomNavigation, BottomNavigationTab, Layout, Text } from '@ui-kitten/components';
+
+const { Navigator, Screen } = createBottomTabNavigator();
+
+const UsersScreen = () => (
+  <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <Text category='h1'>USERS</Text>
+  </Layout>
+);
+
+const OrdersScreen = () => (
+  <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <Text category='h1'>ORDERS</Text>
+  </Layout>
+);
+
+const BottomTabBar = ({ navigation, state }) => (
+  <BottomNavigation
+    selectedIndex={state.index}
+    onSelect={index => navigation.navigate(state.routeNames[index])}>
+    <BottomNavigationTab title='USERS'/>
+    <BottomNavigationTab title='ORDERS'/>
+  </BottomNavigation>
+);
+
+const TabNavigator = () => (
+  <Navigator tabBar={props => <BottomTabBar {...props} />}>
+    <Screen name='Users' component={UsersScreen}/>
+    <Screen name='Orders' component={OrdersScreen}/>
+  </Navigator>
+);
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ApplicationProvider {...eva} theme={eva.dark} >
+      <NavigationContainer>
+        <TabNavigator/>
+      </NavigationContainer>
+    </ApplicationProvider>
   );
 }
 
--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,8 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo']
+    presets: ['babel-preset-expo'],
+    test: /@?(ui-kitten|eva-design).*\.(ts|js)x?$/,
+    loader: 'babel-loader'
   };
 };
